da46e1629895897a3ef2277056c2f05d8ce64f7e
techno-sd <techno-sd@hotmail.com>
2025-12-15 02:00:26 +0400

    feat: enhance templates page UI with new design system

--- components/template-browser.tsx
+++ components/template-browser.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+// Template browser component
 import { useState, useMemo } from 'react'
 import { Card, CardContent, CardHeader } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
